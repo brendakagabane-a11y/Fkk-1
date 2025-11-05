0e01dde1825308dd8574b889d0cbba238d945afc
brendakagabane-a11y <brendakagabane@gmail.com>
2025-11-05 11:55:13 +0300

    Create firebase-config.js

---
+++ config/firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSY0OUQOtY71QT1a4GnP-TNICJ8FNo35M",
+    authDomain: "sr-market-strategies.firebaseapp.com",
+    projectId: "sr-market-strategies",
+    storageBucket: "sr-market-strategies.firebasestorage.app",
+    messagingSenderId: "118475459994",
+    appId: "1:118475459994:web:e8ec9e157abb1ed3ec3e23",
+    measurementId: "G-HZ4E5L32XE"
+};
+
+// ImgBB API Configuration
+const IMGBB_API_KEY = '053eefabdcc58e969e80204a6007a066';
+
+export { firebaseConfig, IMGBB_API_KEY };
